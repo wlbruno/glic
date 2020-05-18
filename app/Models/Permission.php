841150bccb91504085e95df7444f456d3487ad8f
wlbruno <wlbn2121@gmail.com>
2020-05-17 23:28:08 -0300

    erro permission migration

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Permission extends Model
 {
+  
+    protected $table = 'permissions';
+
+
    protected $fillable = ['name', 'description'];
 
     /**
