627679faaf4aedd53e341cd11473dc264c73c096
Willian Bruno Neves Teotônio <wbnt9772130@saude.pe.gov.br>
2020-08-20 11:51:57 -0300

    ajuste do erro da view contato

--- resources/views/site/pages/user/contato.blade.php
+++ resources/views/site/pages/user/contato.blade.php
@@ -1,13 +1,13 @@
 @extends('site.layout.master')
 
 @section('title', 'GLIC')
-@section('titulo', 'Atas Solicitadas')
+@section('titulo', 'Contato ')
 
 @section('content_header')
 
 <ol class="breadcrumb float-sm-right">
      <li class="breadcrumb-item " ><a href="/">Home</a></li>
-   <li class="breadcrumb-item active" ><a href="/">Contato</a></li>
+   <li class="breadcrumb-item active" ><a href="/">Página de Contato</a></li>
 </ol>
 
 @stop
@@ -16,9 +16,6 @@
  <div class="row">
     <div class="col-md-12">
         <div class="card card-primary">
-              <div class="card-header">
-                <h3 class="card-title">Atualizar Perfil</h3>
-              </div>
               <form action="" method="POST">
                  {{ method_field('PUT') }}
                  @csrf
@@ -27,23 +24,26 @@
                   
                   <div class="form-group">
                        <label >Nome:</label>
-                    <input type="text" class="form-control" name="name" placeholder="Nome Completo">
+                    <input type="text" class="form-control" required name="name" placeholder="Nome Completo">
                   </div>
                   
                   <div class="form-group">
                     <label >E-mail:</label>
-                    <input type="email" class="form-control" name="email" placeholder="E-mail">
+                    <input type="email" class="form-control" required name="email" placeholder="E-mail">
                   </div>
 
+                  <div class="col-md-6">
                   <div class="form-group">
                     <label >Telefone:</label>
-                    <input type="number" class="form-control" name="email" placeholder="Telefone">
+                    <input type="number" class="form-control" required name="email" min="0" max="11" placeholder="Telefone">
                   </div>
 
+                  </div>
+                  
                   <div class="form-group">
                    <label for="cnovasenha">Mensagem</label>
                    <div class="col-md-12">
-                              <textarea class="form-control" rows="3" placeholder="Digite sua mensagem"></textarea>
+                              <textarea class="form-control" required rows="3" placeholder="Digite sua mensagem"></textarea>
                             </div>
                   </div>
                 </div>
