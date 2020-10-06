396d025b080c68984337042e6f4d13519c304246
wlbruno <willianbruno.sespe@gmail.com>
2020-10-06 10:50:39 -0300

    ajuste view users

--- resources/views/admin/pages/users/index.blade.php
+++ resources/views/admin/pages/users/index.blade.php
@@ -18,32 +18,43 @@
 
 @section('content')
     <div class="row">
-          <div class="col-lg-3 col-6">
-            <div class="small-box bg-secondary">
-              <div class="inner">
-
-                <p style="text-align: center;font-size: 26px">Usuários pendentes</p>
-                <p style="text-align: center;font-size: 26px"><strong>({{$users}})</strong></p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-bag"></i>
-              </div>
-              <a href="{{ route('users.pendentes') }}" class="small-box-footer">ACESSAR &nbsp;<i class="fas fa-arrow-circle-right"></i></a>
-            </div>
+      <div class="col-lg-3 col-6">
+        <div class="small-box bg-secondary">
+          <div class="inner">
+            <p style="text-align: center;font-size: 26px">Usuários sem permissões</p>
+            <p style="text-align: center;font-size: 26px"><strong>({{$users}})</strong></p>
           </div>
+          <div class="icon">
+            <i class="ion ion-bag"></i>
+          </div>
+            <a href="{{ route('users.pendentes') }}" class="small-box-footer">ACESSAR &nbsp;<i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
 
-          <div class="col-lg-3 col-6">
-            <div class="small-box bg-secondary">
-              <div class="inner">
-
-                <p style="text-align: center;font-size: 26px">Usuários permitidos</p>
-                <p style="text-align: center;font-size: 26px"><strong>({{$users}})</strong></p>
-              </div>
-              <div class="icon">
+        <div class="col-lg-3 col-6">
+          <div class="small-box bg-secondary">
+            <div class="inner">
+              <p style="text-align: center;font-size: 26px">Usuários com permissões</p>
+              <p style="text-align: center;font-size: 26px"><strong>({{$users}})</strong></p>
+            </div>
+            <div class="icon">
                 <i class="ion ion-bag"></i>
-              </div>
+            </div>
               <a href="{{ route('users.permitidos') }}" class="small-box-footer">ACESSAR &nbsp;<i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
+
+          <div class="col-lg-3 col-6">
+            <div class="small-box bg-secondary">
+              <div class="inner">
+                <p style="text-align: center;font-size: 26px">Órgão participantes</p>
+                <p style="text-align: center;font-size: 26px"><strong>({{$users}})</strong></p>
+              </div>
+              <div class="icon">
+                  <i class="ion ion-bag"></i>
+              </div>
+                <a href="{{ route('users.permitidos') }}" class="small-box-footer">ACESSAR &nbsp;<i class="fas fa-arrow-circle-right"></i></a>
+              </div>
+            </div>
       </div>
 @stop 
