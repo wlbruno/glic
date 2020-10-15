a96e5e56de850ae8ce836e9beff91ef5775515fd
wlbruno <willianbruno.sespe@gmail.com>
2020-10-15 10:58:43 -0300

    validacao da carona

--- resources/views/site/pages/atas/licita/index.blade.php
+++ resources/views/site/pages/atas/licita/index.blade.php
@@ -155,7 +155,7 @@
                   </div>
                 @endforeach
                   <div class="card-footer clearfix">
-                    <button type="submit" class="btn btn-sm btn-info float-right">SOLICITAR</button>
+                    <button type="submit"  id="botao" disabled="disabled" class="btn btn-sm btn-info float-right">SOLICITAR</button>
                   </div>
                 </div>
               </form>
@@ -163,3 +163,30 @@
           </div>
 
 @stop
+
+@section('js')
+
+<script>
+// Mantém os inputs em cache:
+var inputs = $('input');
+
+// Chama a função de verificação quando as entradas forem modificadas
+// Usei o 'keyup', mas 'change' ou 'keydown' são também eventos úteis aqui
+inputs.on('keyup', verificarInputs);
+
+function verificarInputs() {
+    var preenchidos = true;  // assumir que estão preenchidos
+    inputs.each(function () {
+        // verificar um a um e passar a false se algum falhar
+        // no lugar do if pode-se usar alguma função de validação, regex ou outros
+        if (!this.value) {
+          preenchidos = true;
+          // parar o loop, evitando que mais inputs sejam verificados sem necessidade
+          return true;
+        }
+    });
+    // Habilite, ou não, o <button>, dependendo da variável:
+    $('button').prop('disabled', !preenchidos); // 
+}
+</script>
+@endsection
